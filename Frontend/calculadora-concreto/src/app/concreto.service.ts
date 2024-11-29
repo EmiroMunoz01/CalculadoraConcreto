d02b78137d7c0ea0253cb3c28eb49162898921dd
EmiroMunoz01 <emiro_2001@icloud.com>
2024-11-29 09:14:41 -0500

    Se ha creado el observable con los datos de concreto

--- Frontend/calculadora-concreto/src/app/concreto.service.ts
+++ Frontend/calculadora-concreto/src/app/concreto.service.ts
@@ -11,11 +11,13 @@ export class ConcretoService {
 
   constructor(private clienteHttp: HttpClient) {}
 
-  //metodo que sera con el que nos vamos a comunicar para obtener todos los objetos de tipo concreto
+  //1 metodo que sera con el que nos vamos a comunicar para obtener todos los objetos de tipo concreto
 
-  //regresaras un objeto de tipo observable, debemos suscribirnos para recuperar los objetos de tipo concreto
+  //2 regresaras un objeto de tipo observable, debemos suscribirnos para recuperar los objetos de tipo concreto
   obtenerProductosLista(): Observable<Concreto[]> {
-    //usaremos el objeto de clienteHttp para llamar la peticion get, ademas estamos indicando que es un arreglo de objetos de tipo concreto, y como parametro pasamos el urlBase, es la url con la que haremos la peticion
+    //3 usaremos el objeto de clienteHttp para llamar la peticion get, ademas estamos indicando que es un arreglo de objetos de tipo concreto, y como parametro pasamos el urlBase, es la url con la que haremos la peticion
+
+    //debemos suscribirnos a este metodo porque devuelve un objeto de tipo observable que retornara todos los objetos de tipo concreto, pero esto lo haremos desde el componente concreto lista
     return this.clienteHttp.get<Concreto[]>(this.urlBase);
   }
 }
