ba64d899ef509363651b1b1918536bd490d95715
EmiroMunoz01 <emiro_2001@icloud.com>
2024-11-29 18:47:15 -0500

    Se ha creado la clase de concreto, y se ha agregado una excepcion en el codigo para evitar errores al usar una variable que no se ha declarado

--- Frontend/calculadora-app/src/app/app.module.ts
+++ Frontend/calculadora-app/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ConcretoListaComponent } from './concreto-lista/concreto-lista.component';
+import { ConcretoComponent } from './concreto/concreto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConcretoListaComponent,
+    ConcretoComponent
   ],
   imports: [
     BrowserModule,
---
+++ Frontend/calculadora-app/src/app/concreto-lista/concreto-lista.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-concreto-lista',
+  templateUrl: './concreto-lista.component.html',
+  styleUrl: './concreto-lista.component.css'
+})
+export class ConcretoListaComponent {
+
+}
