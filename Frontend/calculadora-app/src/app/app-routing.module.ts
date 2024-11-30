2b228fbcfa0f711938ee8501f22292729ed21ede
EmiroMunoz01 <emiro_2001@icloud.com>
2024-11-29 21:36:03 -0500

    hemos eliminado el error del listado

--- Frontend/calculadora-app/src/app/app-routing.module.ts
+++ Frontend/calculadora-app/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ConcretoListaComponent } from './concreto-lista/concreto-lista.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //12 hemos definido la lista por default
+  {path:'concreto', component: ConcretoListaComponent},
+  {path:'', redirectTo: 'concreto', pathMatch:'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
